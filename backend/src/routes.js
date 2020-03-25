f8bb65f8aa109f053926bc44952b7bace3b96001
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-03-25 18:24:00 -0300

    Add create ong route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const crypto = require('crypto')
+
+const connection = require('./database/connection')
 
 const routes = express.Router()
 
-routes.get('/', (req, res)=>{
-    return res.json({
-        events: 'Semana OmniStack 11.0',
-        aluno: 'Guilherme Augusto'  
+routes.post('/ongs', async (request, response)=>{
+    const {name, email, whatsapp, city, uf} = request.body
+
+    const id = crypto.randomBytes(4).toString('HEX')
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
     })
+
+    
+
+    return response.json({ id })
 })
 
 module.exports = routes
