c463aeffd6c4e100d3b1efca0f3bd3cfb145c80c
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-03-25 21:24:27 -0300

    Add ongs incidents route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const OngController = require('./controllers/OngController')
 const IncidentsController = require('./controllers/IncidentsController')
+const ProfileController = require('./controllers/ProfileController')
 
 const routes = express.Router()
 
@@ -11,5 +12,9 @@ routes.post('/ongs', OngController.create)
 routes.post('/incidents', IncidentsController.create)
 routes.get('/incidents', IncidentsController.index)
 routes.delete('/incidents/:id', IncidentsController.delete)
+
+routes.get('/profile/', ProfileController.index)
+
+
 
 module.exports = routes
