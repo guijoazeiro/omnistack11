ccfe88df336ddf434dc3be2d277fbd2657aacb4a
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-03-27 20:03:00 -0300

    Add profile page

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -13,7 +13,7 @@ export default function () {
                     <img src={logoImg} alt="Be The Hero"></img>
 
                     <h1>Cadastro</h1>
-                    <p>Faça seu cadastro, entrena plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
                     Não tenho cadastro
